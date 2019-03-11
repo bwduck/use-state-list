cf26b2a59fb552a91de76f623506620a59931dae
Brad Duck <brad.duck@thinkwrap.com>
2019-03-11 00:26:43 -0400

    Using rollup

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+import { sizeSnapshot } from 'rollup-plugin-size-snapshot';
+import pkg from './package.json';
+
+export default {
+  input: 'src/use-state-list.js',
+  external: ['react', 'react-dom', 'prop-types'],
+  output: [
+    { file: pkg.main, format: 'cjs', exports: 'named' },
+  ],
+  plugins: [
+    resolve(),
+    commonjs({ include: 'node_modules/**' }),
+    babel({ exclude: 'node_modules/**' }),
+    terser(),
+    sizeSnapshot(),
+  ],
+};
--- test/use-state-list.test.js
+++ test/use-state-list.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { configure, mount } from 'enzyme';
 import wait from 'waait';
-import useStateList from './use-state-list';
+import useStateList from '../src/use-state-list';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
